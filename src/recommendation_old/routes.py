70111f5b3d0e47df3146c135572d10d8a3e41d08
Eduardo Luis <eduardo.luis@linx.com.br>
2025-06-15 14:59:18 -0300

    recriação da estrutura de recomendação

---
+++ src/recommendation_old/routes.py
@@ -0,0 +1,15 @@
+from src.recommendation_old.utils import get_bert_recommendations
+from src.recommendation_old.services import get_recommendations
+
+if __name__ == '__main__':
+    recommendations = get_recommendations("Camarão")
+
+    print("Recomendação de Camarão com Vetorização\n")
+    for rec, score in recommendations:
+        print(f"Receita: {rec} Similaridade: {score:.2f}")
+
+    bert_recommendations = get_bert_recommendations("Hamburger")
+
+    print("\nRecomendação de Hamburger com BERT\n")
+    for rec, score in bert_recommendations:
+        print(f"Receita: {rec} Similaridade: {score:.4f}")
